05def66f20b378b1acb0f986329b78c578de37c2
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-08 19:15:11 +0530

    Create Question6.cpp

---
+++ Question6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	       int num;
+           cout<<"\n Enter any number.";
+           cin>>num;
+           if(num%4==0)
+            cout<<"\n Number is a leap year."<<endl;
+           else
+            cout<<"\n Number is not a leap year."<<endl;
+	return 0;
+}
