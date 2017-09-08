7153c06c287ec1a8286fe3bf6409038e77fd1cec
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-08 12:33:03 +0530

    Create Question3.cpp

---
+++ Question3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number;
+	cout<<"\n Enter any number.";
+	cin>>number;
+	if(number>0)
+	 cout<<"\n It is a positive number."<<endl;
+	if(number<0)
+	 cout<<"\n It is a negative number."<<endl;
+	if(number=0)
+	 cout<<"\n It is 0."<<endl;
+
+	return 0;
+}
