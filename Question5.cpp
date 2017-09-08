23c696a5702873c2ed74d92591a5a9ce96442bce
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-08 12:39:43 +0530

    Create Question5.cpp

---
+++ Question5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cout<<"\n Enter any number.";
+	cin>>num;
+	if(num%2==0)
+	 cout<<"\n Number is even."<<endl;
+	else
+	 cout<<"\n Number is odd."<<endl;
+
+	return 0;
+}
