1cfb5f02a17592db851e68067417f3c7e43d160d
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-08 12:23:18 +0530

    Create Question1.cpp

---
+++ Question1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1,num2,maximum;
+	cout<<"Enter two numbers num1,num2";
+	cin>>num1,num2;
+	if(num1>num2)
+	 cout<<"Maximum is="<<num1<<endl;
+	if(num1<num2)
+	 cout<<"Maximum is="<<num2<<endl;
+	if(num1=num2)
+	 cout<<"Both are equal."<<endl;
+	return 0;
+}
+
+
+
+
