aeab2411aba5d75eecffa448ebf994f8771570c2
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-09 20:54:14 +0530

    Create Question27.cpp

---
+++ Question27.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+           int n,i,sum=0;
+           cout<<"\n Enter a positive integer.";
+           cin>>n;
+           for(i=1;i<n;++i)
+           {
+           	  sum+=i;
+           }
+           cout<<"\n Sum is="<<sum<<endl;
+           
+           
+           	  
+                    
+    
+
+    return 0;
+}
