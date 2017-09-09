00bbe4c1f71377334e055771f81fe93f55ffd6b9
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-09 20:42:15 +0530

    Create Question25.cpp

---
+++ Question25.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+           int counter;
+           cout<<"\n Enter numbers between 1 to 100.";
+           for(counter=1;counter<=100;counter++)
+           {
+           	  if(counter%2==0)
+           	  cout<<"\n Counter"<<endl;
+           }
+           
+           
+           	  
+                    
+    
+
+    return 0;
+}
