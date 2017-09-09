c157dbf516512dcf5fec019c30097e3823669a44
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-09 20:26:54 +0530

    Create Question23.cpp

---
+++ Question23.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+           int i,n;
+           cout<<"\n Enter all natural numbers from n to 1";
+           cin>>n;
+           i>0;
+           while (i>0)
+           {
+           	  cout<<"\n n"<<endl;
+           	  n--;
+           }
+                    
+    
+
+    return 0;
+}
