2e49fa5c3301350f526e56972ada97d509dc597b
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-09 21:00:39 +0530

    Create Question28.cpp

---
+++ Question28.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+           int counter,n,sum;
+           cout<<"\n Enter a positive number.";
+           cin>>n;
+           for(counter=1;counter<=n;counter++)
+           {
+           	  if(counter%2==0);
+           	  sum=sum+counter;
+           }
+           cout<<"\n Sum of all even numbers:"<<sum<<endl;
+           
+           
+           
+           	  
+                    
+    
+
+    return 0;
+}
