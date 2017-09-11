1e3905cfc65be91fc94bdad8b3998baeac946563
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-11 23:35:02 +0530

    Create Question41.cpp

---
+++ Question41.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+          int i;
+          char ch;
+          cout<<"\n \n Programme to print all the ASCII characters.";
+          cout<<"\n The ASCII characters are as follows=";
+          for(i=1;i<256;i++)
+          {
+          	 cout<<"\n"<<i<<"t"<<ch;
+          	 ch++;
+          }
+          return 0;
+}
