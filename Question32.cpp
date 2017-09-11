8ae83ee5c84d40f481412454b92797070693317f
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-11 19:38:52 +0530

    Create Question32.cpp

---
+++ Question32.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+           int n,lastdigit;
+           cout<<"\n Enter any number";
+           cin>>n;
+           lastdigit=n%10;
+           cout<<"\n Last digit is="<<lastdigit<<endl;
+	return 0;
+}
