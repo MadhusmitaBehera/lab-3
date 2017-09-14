3b193d496096a45874f2438c8dbe421f0e06f553
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-14 17:02:07 +0530

    Create Question71.cpp

---
+++ Question71.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+ int integer;
+ std::cout<<"\n Enter hex to convert hex to decimal.";
+ std::cin>>std::hex>>integer;
+ std::cout<<integer<<std::endl;
+ return 0;
+}
